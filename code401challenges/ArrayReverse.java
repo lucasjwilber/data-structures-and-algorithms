7bb5a070afaceb3115897bc479c699bf0618e7f4
Lucas Wilber <lucasjwilber@gmail.com>
2020-01-06 18:18:47 -0800

    added comments

--- code401challenges/ArrayReverse.java
+++ code401challenges/ArrayReverse.java
@@ -2,8 +2,11 @@ import java.util.Arrays;
 
 public class ArrayReverse {
   public static int[] reverseArray (int[] inputArray) {
+    //make an empty array with the same length as the input array:
     int inputLength = inputArray.length;
     int[] reversedArray = new int[inputLength];
+
+    //loop over the input array in reverse:
     int count = 0;
     for (int i = inputLength - 1; i >= 0; i--) {
       reversedArray[count] = inputArray[i];
@@ -13,6 +16,7 @@ public class ArrayReverse {
   }
   public static void main (String[] args) {
     int[] testArray = new int[] {2, 6, 7, 4, 11, 2323};
-    System.out.println(Arrays.toString(reverseArray(testArray)));
+    System.out.println("reversing array " + Arrays.toString(testArray));
+    System.out.println("result: " + Arrays.toString(reverseArray(testArray)));
   }
 }
