e1adfceeed03ad1b84c6ebfe4f7477b7a8d5b486
Lucas Wilber <lucasjwilber@gmail.com>
2020-01-07 18:53:51 -0800

    challenge completed but need to write tests

---
+++ code401challenges/src/main/java/code401challenges/java/ArrayShift.java
@@ -0,0 +1,36 @@
+package code401challenges.java;
+
+import java.util.Arrays;
+
+public class ArrayShift {
+
+    public static int[] insertShiftArray(int[] inputArray, int number) {
+        int[] newArray = new int[inputArray.length + 1];
+        int middleIndex = (int) Math.ceil(newArray.length / 2);
+        int position = 0;
+
+        for (int i = 0; i < newArray.length; i++) {
+            if (i == middleIndex) {
+                continue;
+            }
+            newArray[i] = inputArray[position];
+            position++;
+        }
+
+        newArray[middleIndex] = number;
+
+        return newArray;
+    }
+
+    public static void main(String[] args) {
+        int[] testArr = {1, 2, 3, 6};
+        int testNum = 5;
+        System.out.println("Result with " + Arrays.toString(testArr) + " and number " + testNum);
+        System.out.println(Arrays.toString(insertShiftArray(testArr, testNum)));
+
+        int [] testArr2 = {3, 5, 1, 6, 13};
+        testNum = 69;
+        System.out.println("Result with " + Arrays.toString(testArr2) + " and number " + testNum);
+        System.out.println(Arrays.toString(insertShiftArray(testArr2, testNum)));
+    }
+}
