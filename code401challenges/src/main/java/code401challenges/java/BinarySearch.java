9b2239afcd00765ac41b23340c7331066b5054f3
Lucas Wilber <lucasjwilber@gmail.com>
2020-01-08 19:05:14 -0800

    added a simple test

--- code401challenges/src/main/java/code401challenges/java/BinarySearch.java
+++ code401challenges/src/main/java/code401challenges/java/BinarySearch.java
@@ -1,4 +1,35 @@
 package code401challenges.java;
 
+//need to add handling for non-int array values
 public class BinarySearch {
+    public static int binarySearchForValue(int[] sortedArray, int searchKey) {
+        int currentIndex = sortedArray.length / 2;
+        int currentNumber = sortedArray[currentIndex];
+        int cycleCount = 0;
+
+        while (currentNumber != searchKey) {
+            cycleCount++;
+            if (searchKey > currentNumber) {
+                currentIndex += (sortedArray.length - currentIndex) / 2;
+            }
+            if (searchKey < currentNumber) {
+                currentIndex -= (sortedArray.length - currentIndex) / 2;
+            }
+            currentNumber = sortedArray[currentIndex];
+
+            //if number is not in array:
+            if (
+                    currentIndex == 0 && currentNumber != searchKey ||
+                            currentIndex == sortedArray.length - 1 && currentNumber != searchKey
+            ) {
+                System.out.println("The given number is not in the array");
+                return -1;
+            }
+        }
+        System.out.println("It took " + cycleCount + " cycles to find the index.");
+        return currentIndex;
+    }
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
 }
--- code401challenges/src/test/java/code401challenges/java/BinarySearchTest.java
+++ code401challenges/src/test/java/code401challenges/java/BinarySearchTest.java
@@ -1,37 +1,17 @@
 package code401challenges.java;
 
+import org.junit.Test;
+
+import java.sql.SQLOutput;
+
+import static org.junit.Assert.*;
+
+
 public class BinarySearchTest {
 
-    public static int binarySearchForValue(int[] sortedArray, int searchKey) {
-        int currentIndex = sortedArray.length / 2;
-        int currentNumber = sortedArray[currentIndex];
-
-        while (currentNumber != searchKey) {
-            if (searchKey > currentNumber) {
-                currentIndex += (sortedArray.length - currentIndex) / 2;
-            }
-            if (searchKey < currentNumber) {
-                currentIndex -= (sortedArray.length - currentIndex) / 2;
-            }
-            currentNumber = sortedArray[currentIndex];
-
-            //if number is not in array:
-            if (
-                currentIndex == 0 && currentNumber != searchKey ||
-                currentIndex == sortedArray.length - 1 && currentNumber != searchKey
-            ) {
-                System.out.println("The given number is not in the array");
-                return -1;
-            }
-        }
-        return currentIndex;
-    }
-
-
-
-
-    public static void main (String[] args) {
+    @Test public void testBinarySearch() {
         int[] testArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println(binarySearchForValue(testArray, 4));
+        assertEquals(0, BinarySearch.binarySearchForValue(testArray, 1));
     }
+
 }
