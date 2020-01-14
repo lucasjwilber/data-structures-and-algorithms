29b2dc1eff29be6da32fc01af2c9930889a5e69a
Lucas Wilber <lucasjwilber@gmail.com>
2020-01-14 11:05:33 -0800

    moved LL files into this repo

--- code401challenges/src/main/java/LinkedList/LinkedList.java
+++ code401challenges/src/main/java/LinkedList/LinkedList.java
@@ -1,4 +1,76 @@
 package LinkedList;
 
 public class LinkedList {
+    public Node head = null;
+
+    public class Node {
+        public int value;
+        public Node next;
+        public Node(int value) {
+            this.next = null;
+            this.value = value;
+        }
+
+        public String toString() {
+            return "{ " + this.value + " } -> " + (this.next == null ? "NULL" : this.next.toString());
+        }
+    }
+
+    public void insert(int value) {
+        Node newNode = new Node(value);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public boolean includes(int value) {
+        boolean answer = false;
+        Node current = this.head;
+        while (current.next != null) {
+            if (current.value == value) {
+                answer = true;
+                break;
+            }
+            current = current.next;
+        }
+        return answer;
+    }
+
+    public void append(int value) {
+        Node current = this.head;
+        Node newNode = new Node(value);
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    public void insertBefore(int value, int newVal) throws ArrayIndexOutOfBoundsException {
+        if (this.includes(value)) {
+            Node current = this.head;
+            Node newNode = new Node(value);
+
+            while (current.next != null) {
+                if (current.value == value) {
+                    newNode.next = current.next;
+                    current.next = newNode;
+                    current.value = newVal;
+                    break;
+                }
+                current = current.next;
+            }
+        }
+    }
+
+    public void insertAfter(int value, int newVal) {
+        Node current = this.head;
+        Node newNode = new Node(newVal);
+        while (current.next != null) {
+            if (current.value == value) {
+                newNode.next = current.next;
+                current.next = newNode;
+                break;
+            }
+            current = current.next;
+        }
+    }
 }
--- code401challenges/src/test/java/code401challenges/java/LinkedListTest.java
+++ code401challenges/src/test/java/code401challenges/java/LinkedListTest.java
@@ -1,4 +1,135 @@
 package code401challenges.java;
 
+import LinkedList.LinkedList;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
 public class LinkedListTest {
+    public int getListLength(LinkedList list) {
+        if (list.head == null) {
+            return 0;
+        } else if (list.head.next == null) {
+            return 1;
+        } else {
+            int length = 1;
+            LinkedList.Node current = list.head;
+            while (current.next != null) {
+                length++;
+                current = current.next;
+            }
+            return length;
+        }
+    }
+
+//    LinkedList testList;
+//    @Before
+//    public void setUp() {
+//        testList = new LinkedList();
+//        testList.insert(1);
+//        testList.insert(3);
+//        testList.insert(43);
+//        testList.insert(7);
+//        testList.insert(2345);
+//        testList.insert(324);
+//        testList.insert(637);
+//        testList.insert(45);
+//        testList.insert(0);
+//        testList.insert(0);
+//    }
+
+    @Test public void canCreateAnEmptyLinkedList() {
+        LinkedList newList = new LinkedList();
+        assertNull(newList.head);
+    }
+    @Test public void checkInsert() {
+        LinkedList testList = new LinkedList();
+        testList.insert(1);
+        testList.insert(3);
+        testList.insert(3);
+        testList.insert(7);
+        testList.insert(9);
+
+        assertTrue(testList.head.value == 9);
+        assertEquals(getListLength(testList), 5);
+    }
+    @Test public void checkHeadIsLastNodeAdded() {
+        LinkedList testList = new LinkedList();
+        testList.insert(4);
+        testList.insert(8);
+
+        assertTrue(testList.head.value == 8);
+    }
+    @Test public void testIncludesInsertedValues() {
+        LinkedList testList = new LinkedList();
+        testList.insert(23);
+        testList.insert(3453);
+        testList.insert(253);
+        testList.insert(23456);
+        testList.insert(9);
+
+        assertTrue(testList.includes(23456));
+        assertFalse(testList.includes(55));
+    }
+    @Test public void testOnlyIncludesInsertedValues() {
+        LinkedList testList = new LinkedList();
+        testList.insert(23);
+        testList.insert(3453);
+        testList.insert(253);
+        testList.insert(23456);
+        testList.insert(9);
+
+        assertFalse(testList.includes(55));
+    }
+    @Test public void testToString() {
+        LinkedList testList = new LinkedList();
+        testList.insert(1);
+        testList.insert(2);
+        testList.insert(3);
+        testList.insert(4);
+        testList.insert(5);
+    }
+    @Test public void listToStringContainsAllValues() {
+        LinkedList testList = new LinkedList();
+        testList.insert(135);
+        testList.insert(4);
+        testList.insert(6);
+        testList.insert(7);
+        testList.insert(65);
+
+        assertTrue(testList.head.toString().contains("135"));
+        assertTrue(testList.head.toString().contains("4"));
+        assertTrue(testList.head.toString().contains("6"));
+        assertTrue(testList.head.toString().contains("7"));
+        assertTrue(testList.head.toString().contains("65"));
+    }
+    @Test public void checkAppendAddsToEnd() {
+        LinkedList testList = new LinkedList();
+        testList.insert(135);
+        testList.insert(4);
+        testList.append(1);
+
+        assertEquals(1, testList.head.next.next.value);
+    }
+    @Test public void checkInsertBefore() {
+        LinkedList testList = new LinkedList();
+        testList.insert(5);
+        testList.insert(135);
+        testList.insert(35);
+        testList.insert(22);
+        testList.insertBefore(135, 4);
+
+        System.out.println(testList.head.toString());
+    }
+
+    @Test public void checkInsertAfter() {
+        LinkedList testList = new LinkedList();
+        testList.insert(135);
+        testList.insert(4);
+        testList.insert(6);
+        testList.insertAfter(4, 99);
+        //6 4 99 135
+        assertEquals(99, testList.head.next.next.value);
+    }
 }
