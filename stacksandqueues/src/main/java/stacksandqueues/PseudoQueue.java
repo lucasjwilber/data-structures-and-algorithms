54f0992f20c3bf3b1f5ee23099d9372337b7b54d
Lucas Wilber <lucasjwilber@gmail.com>
2020-01-20 18:58:16 -0800

    added the PseudoQueue and its enqueue/dequeue methods

---
+++ stacksandqueues/src/main/java/stacksandqueues/PseudoQueue.java
@@ -0,0 +1,57 @@
+package stacksandqueues;
+
+public class PseudoQueue {
+    public Stack<Integer> front = new Stack<Integer>();
+    public Stack<Integer> rear = new Stack<Integer>();
+
+    public void enqueue(int value) {
+        rear.push(value);
+
+        if (front.top == null) {
+            front.push(value);
+        } else {
+            //flip front into a temp stack
+            Stack<Integer> tempStack = new Stack<Integer>();
+            while (front.top != null) {
+                tempStack.push(front.pop());
+            }
+            //add the new Node
+            tempStack.push(value);
+            //flip the temp stack back into front
+            while (tempStack.top != null) {
+                front.push(tempStack.pop());
+            }
+        }
+    }
+
+    public int dequeue() throws NullPointerException {
+
+        Stack<Integer> tempStack = new Stack<Integer>();
+        //flip rear into a temp stack
+        while (rear.top != null) {
+            tempStack.push(rear.pop());
+        }
+        //pop off the end Node
+        tempStack.pop();
+        //flip temp stack back into rear
+        while (tempStack.top != null) {
+            rear.push(tempStack.pop());
+        }
+
+        return front.pop();
+    }
+
+    public static void main(String[] args) {
+        PseudoQueue q = new PseudoQueue();
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        q.enqueue(4);
+
+        System.out.println(q.dequeue());
+        System.out.println(q.dequeue());
+        System.out.println(q.dequeue());
+        System.out.println(q.dequeue());
+
+    }
+}
