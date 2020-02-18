ff5c011fa0edcbd7d132a8ce62ea2cf82b44ccfe
Lucas Wilber <lucasjwilber@gmail.com>
2020-02-18 13:10:43 -0800

    removed main(), fixed a string comparison

--- utilities/src/main/java/utilities/RepeatedWord.java
+++ utilities/src/main/java/utilities/RepeatedWord.java
@@ -4,7 +4,7 @@ import java.util.Hashtable;
 
 public class RepeatedWord {
     public static String repeatedWord(String input) {
-        if (input == null || input == "") return null;
+        if (input == null || input.equals("")) return null;
         String[] words = input.split(" ");
         int highestCount = 0;
         String mostCommonWord = null;
@@ -31,11 +31,5 @@ public class RepeatedWord {
             }
         }
         return mostCommonWord;
-    }
-
-    public static void main(String[] args) {
-        String example1 = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...";
-        System.out.println(repeatedWord(example1));
-        System.out.println(repeatedWord("the is but is man is"));
     }
 }
